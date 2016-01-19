9c17dc2d2d93652ae2d56864691b062a6cef00d1
Corentin Cocoual <corentin.cocoual@gmail.com>
2016-01-19 15:53:25 +0100

    Intégration du template Ajout des menus Intégration locale des librairies du template

--- client/app/app.routes.js
+++ client/app/app.routes.js
@@ -1,11 +1,50 @@
 qrApp.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/");
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      controller: 'HomeCtrl',
-      templateUrl: "./app/components/home/home.tpl.html"
-    });
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            controller: 'HomeCtrl',
+            templateUrl: "./app/components/home/home.tpl.html"
+        });
+
+
+    //Routes pour les vues relatives aux cours
+    $stateProvider
+        .state('cours', {
+            url: '/cours'/**,
+            abstract:true,
+             controller: 'HomeCtrl',
+             templateUrl: "./app/components/home/home.tpl.html"**/
+        })
+        .state('cours.list', {
+            url: '/list'
+        })
+        .state('cours.create', {
+            url: '/create'
+        });
+
+    //Routes pour les vues relatives aux étudiants
+    $stateProvider
+        .state('etudiant', {
+            url: '/etudiant'/**,
+             abstract:true,
+             controller: 'HomeCtrl',
+             templateUrl: "./app/components/home/home.tpl.html"**/
+        })
+        .state('etudiant.list', {
+            url: '/list'
+        })
+        .state('etudiant.create', {
+            url: '/create'
+        });
+
+    //Routes pour les vues relatives aux statistiques
+    $stateProvider
+        .state('statistiques', {
+            url: '/statistiques'/**,
+             controller: 'HomeCtrl',
+             templateUrl: "./app/components/home/home.tpl.html"**/
+        });
 });
