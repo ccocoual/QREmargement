f25d982817478552eb3ee26e4fe4eab10e86b4d2
Meunier Maxime <mxmeunier@gmail.com>
2016-01-19 16:27:11 +0100

    Suppression de l'autoload des étudiants.

--- client/app/components/student/student.ctrl.js
+++ client/app/components/student/student.ctrl.js
@@ -6,21 +6,11 @@ StudentCtrl.$inject = ['StudentFactory'];
 
 function StudentCtrl(StudentFactory) {
     var vm = this;
-    vm.test = "toto";
     vm.students = [];
-
-    activate();
-
-    function activate() {
-        return getStudents().then(function() {
-            console.log("log");
-        });
-    }
     
     function getStudents() {
         return StudentFactory.getStudents()
             .then(function(data) {
-                console.log(data);
                 vm.students = data;
                 return vm.students;
             });
