773415d7a3ca13cf8977d600a29f6a87877054d9
Meunier Maxime <mxmeunier@gmail.com>
2016-01-19 16:09:51 +0100

    Suppression des test CRUD dans home

--- client/app/components/home/home.ctrl.js
+++ client/app/components/home/home.ctrl.js
@@ -1,17 +1,6 @@
-
 'use strict';
 
 qrApp.controller('HomeCtrl', function($scope, $http) {
-  console.log("Welcome Home ! ");
-    $http({
-        method: 'GET',
-        url: 'http://localhost:3000/posts'
-    }).then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
-        console.log(response);
-        $scope.posts = response;
-    }, function errorCallback(response) {
-        console.log("REST error");
-  });
+    console.log("Welcome Home ! ");
 });
+
