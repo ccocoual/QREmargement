179fb67d794eb1384d3b0b784c3a20d07adab455
Meunier Maxime <mxmeunier@gmail.com>
2016-01-29 16:05:56 +0100

    Filling class views and create view form

--- client/app/components/class/class.ctrl.js
+++ client/app/components/class/class.ctrl.js
@@ -3,11 +3,12 @@
 
     qrApp.controller('ClassCtrl', ClassCtrl);
 
-    ClassCtrl.$inject = ['ClassFactory'];
+    ClassCtrl.$inject = ['ClassFactory', "$state"];
 
     function ClassCtrl(ClassFactory) {
         var vm = this;
         vm.classes = [];
+        vm.newClass = {};
         
         vm.getClasses = function() {
             return ClassFactory.getClasses()
@@ -15,6 +16,11 @@
                     vm.classes = data;
                     return vm.classes;
                 });
+        }
+        
+        vm.createClass = function() {
+            return ClassFactory.createClass(vm.newClass);
+            $state.go('class.list');
         }
     }
 })();
