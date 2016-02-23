ddd209dbace508c3d76b4d167e0e6544068cb625
Meunier Maxime <mxmeunier@gmail.com>
2016-02-23 16:11:13 +0100

    Adding constant for authentication events

--- client/app/app.constants.js
+++ client/app/app.constants.js
@@ -2,3 +2,12 @@
 qrApp.constant('RESTURL', 'http://148.60.11.185:8080/test/');
 qrApp.constant('WEBAPPURL', 'http://localhost:63342/client/index.html#/');
 //qrApp.constant('WEBAPPURL', 'http://148.60.11.185/client/index.html#/');
+
+qrApp.constant('AUTH_EVENTS', {
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+})
