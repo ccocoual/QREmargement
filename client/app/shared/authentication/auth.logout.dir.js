50f8a23f76623c770ea01adcffdcdd5e3bbc3bae
Meunier Maxime <mxmeunier@gmail.com>
2016-02-26 18:47:19 +0100

    Modify teacher logout directive

--- client/app/shared/authentication/auth.logout.dir.js
+++ client/app/shared/authentication/auth.logout.dir.js
@@ -5,14 +5,6 @@
         return {
             restrict: 'A',
             template: '<div ng-if="visible" ng-include="\'app/shared/authentication/auth.teacher.logout.tpl.html\'">',
-            link: function (scope) {
-                var showDialog = function () {
-                    scope.visible = true;
-                };
-
-                scope.visible = false;
-                scope.$on(AUTH_EVENTS.loginSuccess, showDialog);
-            }
         };
     })
     
