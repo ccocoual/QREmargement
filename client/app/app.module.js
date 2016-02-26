2056434ca8f6ceafa75dd80df5f10b8bfbfaf99d
Meunier Maxime <mxmeunier@gmail.com>
2016-02-26 15:29:57 +0100

    Adding stateOnChange for authentication handling

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -14,6 +14,15 @@ var qrApp = angular.module("QRApp", [
     "ngTable"
 ]);
 
+qrApp.run(function ($rootScope, AUTH_EVENTS, AuthFactory) {
+    $rootScope.$on('$stateChangeStart', function (event, next) {
+        if (!AuthFactory.isAuthenticated()) {
+            event.preventDefault();
+            $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+        }
+    });
+})
+
 qrApp.config(['ChartJsProvider', function (ChartJsProvider) {
     ChartJsProvider.setOptions({
         colours: ['#4abebc', '#f54b50'],
