dfbd3a65d9dceb5e9d04f4f44f5f25af095db2ea
Meunier Maxime <mxmeunier@gmail.com>
2016-02-26 15:30:58 +0100

    Adding interceptor on isAuthenticated event

--- client/app/shared/authentication/auth.interceptor.fact.js
+++ client/app/shared/authentication/auth.interceptor.fact.js
@@ -3,15 +3,24 @@
 
     qrApp.factory('AuthInterceptor', AuthInterceptor);
 
-    function AuthInterceptor($rootScope, $q, AUTH_EVENTS) {
+    function AuthInterceptor($rootScope, $q, AUTH_EVENTS, $state) {
         return {
-            responseError: function (response) { 
-                $rootScope.$broadcast({
-                    401: AUTH_EVENTS.notAuthenticated,
-                    403: AUTH_EVENTS.notAuthorized
-                }[response.status], response);
-                return $q.reject(response);
-            }
-        };   
+            responseError: responseError,
+            redirectLogin: redirectLogin
+        }; 
+        
+        function responseError(response) {
+            $rootScope.$broadcast({
+                401: AUTH_EVENTS.notAuthenticated,
+                403: AUTH_EVENTS.notAuthorized
+            }[response.status], response);
+            return $q.reject(response);
+        }
+        
+        function redirectLogin() {
+            $rootScope.$on(AUTH_EVENTS.notAuthenticated, function() {
+                $state.go("auth_teacher");
+            })
+        }
     }
 })();
